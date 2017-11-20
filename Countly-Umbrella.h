066d9b14c13da96bb32b6f7183a9e17b1c931eef
dorlov <orlov.dmtr@gmail.com>
2017-11-20 17:38:55 +0300

    Add carthage support

---
+++ Countly-Umbrella.h
@@ -0,0 +1,20 @@
+//
+//  Countly.h
+//  Countly
+//
+//  Created by Dmitry Orlov on 11/20/17.
+//  Copyright © 2017 wirecard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Countly.
+FOUNDATION_EXPORT double CountlyVersionNumber;
+
+//! Project version string for Countly.
+FOUNDATION_EXPORT const unsigned char CountlyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Countly/PublicHeader.h>
+#import "Countly.h"
+
+
